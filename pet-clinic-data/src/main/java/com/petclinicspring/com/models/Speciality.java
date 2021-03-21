4a8ef950507dd6374a3406fd78180c54dee0fc1e
terrorRuntime <kishorbundhe@gmail.com>
2021-03-22 00:11:41 +0530

    create vet speciality Entity,Associate To Vet .Closes #30
    Implemented Speciality and associated to Vet

---
+++ pet-clinic-data/src/main/java/com/petclinicspring/com/models/Speciality.java
@@ -0,0 +1,13 @@
+package com.petclinicspring.com.models;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
