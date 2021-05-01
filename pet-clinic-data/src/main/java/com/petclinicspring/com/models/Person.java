13655159de785aca24ad5d469180dc76fb774829
terrorRuntime <kishorbundhe@gmail.com>
2021-05-01 19:49:58 +0530

    Closes #38
    Converted Owners to JPA entity

--- pet-clinic-data/src/main/java/com/petclinicspring/com/models/Person.java
+++ pet-clinic-data/src/main/java/com/petclinicspring/com/models/Person.java
@@ -1,19 +1,29 @@
 package com.petclinicspring.com.models;
 
-public class Person extends BaseEntity{
-	private String firstName;
-	private String lastName;
-	public String getFirstName() {
-		return firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
--- pet-clinic-data/src/main/java/com/petclinicspring/com/models/Pet.java
+++ pet-clinic-data/src/main/java/com/petclinicspring/com/models/Pet.java
@@ -1,11 +1,22 @@
 package com.petclinicspring.com.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birDate;
 
     public PetType getPetType() {
