a2a7874029e0d1027d0faf7a9edefac753c44e59
terrorRuntime <kishorbundhe@gmail.com>
2021-05-03 23:59:12 +0530

    Closes #52
    Implemenetd then JPA services for all Owners,Pets,PetTyPe,Vets

---
+++ pet-clinic-data/src/main/java/com/petclinicspring/com/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,45 @@
+package com.petclinicspring.com.services.springdatajpa;
+
+import com.petclinicspring.com.models.Pet;
+import com.petclinicspring.com.models.Pet;
+import com.petclinicspring.com.repositories.PetRepository;
+import com.petclinicspring.com.services.PetService;
+import com.petclinicspring.com.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
